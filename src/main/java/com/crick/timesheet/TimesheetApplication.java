f369e9a1e0fb5ee473f035b2aca28cd7ee7849c4
ccarducci <cristiano.carducci@gmail.com>
2019-10-31 22:57:38 +0100

    MongoDB Configuration

--- src/main/java/com/crick/timesheet/TimesheetApplication.java
+++ src/main/java/com/crick/timesheet/TimesheetApplication.java
@@ -1,5 +1,10 @@
 package com.crick.timesheet;
 
+import com.crick.timesheet.entity.Customer;
+import com.crick.timesheet.repository.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -10,6 +15,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class TimesheetApplication {
 
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private  CustomerRepository customerRepository;
+
+
     public static void main(String[] args) {
         SpringApplication.run(TimesheetApplication.class, args);
     }
@@ -17,6 +28,15 @@ public class TimesheetApplication {
     @RequestMapping("/")
     @ResponseBody
     String home() {
+        return "Funziona, forza crick yahoooo!";
+    }
+
+    @RequestMapping("/insert")
+    @ResponseBody
+    String insert() {
+        Customer customerToInsert = new Customer();
+        customerToInsert.firstName = "Cristiano";
+        customerRepository.insert(customerToInsert);
         return "Funziona, forza crick yahoooo!";
     }
 }
